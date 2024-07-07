96c1110b1fcbd63b4acd0cab1e7217280530df07
zhanxiangzong <52497769+zhanxiangzong@users.noreply.github.com>
2024-07-07 22:01:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,9 +29,10 @@ def plot_fatty_acid_content(fatty_acid_types, data_dict, use_name):
         # Perform t-tests and annotate significance for each fatty acid type
         if group != 'WT':
             ## ttest_ind是假设等方差的。和我们的需求完全匹配吗？三个WT之间的方差、3个MUT之间的方差应该假设一致吗？
-            #p_values = [ttest_ind(data_dict['WT'][j], data_dict[group][j]).pvalue for j in range(num_fatty_acids)]
-            # 先改用不等方差的
-            p_values = [ttest_ind(data_dict['WT'][j], data_dict[group][j],equal_var=False).pvalue for j in range(num_fatty_acids)]
+            # 暂时先这样，后面再讨论。
+            p_values = [ttest_ind(data_dict['WT'][j], data_dict[group][j]).pvalue for j in range(num_fatty_acids)]
+            # 改用不等方差的
+            #p_values = [ttest_ind(data_dict['WT'][j], data_dict[group][j],equal_var=False).pvalue for j in range(num_fatty_acids)]
             # equal_var=False
             for rect, p, std in zip(rects, p_values, stds[group]):
                 height = rect.get_height()
